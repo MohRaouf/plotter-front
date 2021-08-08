07ab7bfa34a49cc334d442f7bbe1f349a127d0b3
MohRaouf <moh.proff@gmail.com>
2021-08-08 19:23:05 +0200

    add draggable elements - ng2 charts - working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Plotter-Front';
+export class AppComponent implements OnInit {
+  title = 'Plotter';
+
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+  ];
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: (ChartOptions |{ annotation: any}) = {
+    responsive: true,
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartPlugins = [];
+
+  columns = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep',
+    'Fall asleep'
+
+  ];
+  measurements=[
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
+  ]
+
+  dimensions = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+  constructor() { }
+  ngOnInit(): void {
+
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  console.log(this.dimensions,this.measurements)
+  }
+  clearDimensions(){
+    this.columns=[...this.columns,...this.dimensions];
+    this.dimensions=[];
+  }
+  clearMeasurements(){
+    this.columns=[...this.columns,...this.measurements];
+    this.measurements=[];
+  }
 }
